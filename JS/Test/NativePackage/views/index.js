42f9a2f46e1afc0a6f1a8275a4eec9ff1f0998c5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/Test/NativePackage/views/index.js
@@ -0,0 +1,19 @@
+﻿NativePackage.Index = function (params) {
+    var db = {
+        sampleData: new DevExpress.data.ODataContext({
+            url: "http://services.odata.org/Northwind/Northwind.svc",
+            errorHandler: function (error) {
+                alert(error.message);
+            },
+            entities: {
+                Categories: {
+                    key: 'CategoryID'
+                }
+            }
+        })
+    };
+    var viewModel = {
+        dataSourceCategories: db.sampleData.Categories
+    };
+    return viewModel;
+};
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to build a native package in Visual Studio
+
+
+<p>This example was created for the <a href="https://www.devexpress.com/Support/Center/p/KA18680">How to build a native package</a> article.</p>
+
+<br/>
+
+
